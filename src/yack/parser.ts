238467d7ff83f3d0beb0a23a5534fa4dfa32a722
Michael Bolin <bolinfest@gmail.com>
2022-05-30 21:30:34 -0700

    add support for basic if/else/endif for dialog choices

--- src/yack/parser.ts
+++ src/yack/parser.ts
@@ -97,10 +97,10 @@ class Parser {
         return this.knots;
     }
 
-    private parseChoiceExpr(choiceToken: ChoiceToken): SimpleChoice | null {
+    private parseChoiceExpr(choiceToken: ChoiceToken): SimpleChoice | ComplexChoice {
         const nextToken = this.nextToken();
         if (nextToken == null) {
-            throw this.parseError('expected some token following `*` for choice', choiceToken);
+            this.throwParseError('expected some token following `*` for choice', choiceToken);
         }
 
         switch (nextToken.type) {
@@ -141,26 +141,91 @@ class Parser {
                 };
             }
             case 'control_flow': {
-                if (nextToken.keyword !== 'if') {
-                    throw this.parseError('control flow expr must start with `if`', nextToken);
-                }
-                return this.parseIfExpr(nextToken);
+                return this.parseComplexChoice();
             }
             default:
-                throw this.parseError(`did not expect token of type \`${nextToken.type}\` following \`*\``, nextToken);
+                this.throwParseError(`did not expect token of type \`${nextToken.type}\` following \`*\``, nextToken);
         }
     }
 
-    private parseIfExpr(ifToken: ControlFlowToken) {
-        // parse conditional
-        // parse control flow body
-        // should return when encounter elif, else, or endif?
-        // upon endif, return?
-        return null;
+    /** `this.currentToken` must be the first token for the ComplexChoice. */
+    private parseComplexChoice(): ComplexChoice {
+        const token = this.currentToken;
+        if (token == null) {
+            throw Error('no tokens for choice');
+        }
+
+        switch (token.type) {
+            case 'control_flow': {
+                if (token.keyword !== 'if') {
+                    this.throwParseError(`expected \`if\` keyword but was \`${token.keyword}\``, token);
+                }
+
+                const ifCondition = this.nextToken();
+                if (ifCondition?.type !== "condition") {
+                    this.throwParseError('if must be followed by a condition', token);
+                }
+
+                // Ensure next token is current one before calling into parseComplexChoice().
+                this.nextToken();
+                // This requires no empty if-blocks?
+                const consequent = this.parseComplexChoice();
+
+                const nextToken = this.nextToken();
+                let alternate = null;
+                if (nextToken == null) {
+                    this.throwParseError('missing else or endif after if', token);
+                } else if (nextToken.type !== 'control_flow') {
+                    this.throwParseError('expect else or endif to close if', nextToken);
+                } else if (nextToken.keyword !== 'else' && nextToken.keyword !== 'endif') {
+                    this.throwParseError('expect else or endif to close if', nextToken);
+                } else if (nextToken.keyword === 'endif') {
+                    // All done!
+                } else if (nextToken.keyword === 'else') {
+                    // Ensure next token is current one before calling into parseComplexChoice().
+                    this.nextToken();
+                    alternate = this.parseComplexChoice();
+                    const endifToken = this.nextToken();
+                    if (endifToken?.type !== 'control_flow' || endifToken.keyword !== 'endif') {
+                        this.throwParseError('else missing closing endif', nextToken);
+                    }
+                }
+
+                return {
+                    type: 'control_flow_choice',
+                    condition: ifCondition.expr,
+                    consequent,
+                    alternate,
+                };
+            }
+            case 'string': {
+                return this.parseUnconditionalChoice();
+            }
+        }
+        this.throwParseError(`could not parse token for choice: type ${token.type}`, token);
     }
 
-    private parseError(message: string, position: {line: number; column: number}): Error {
-        return new Error(`parse error on line ${position.line + 1}: ${message}`);
+    private parseUnconditionalChoice(): UnconditionalChoice {
+        if (this.currentToken?.type !== "string") {
+            throw new Error('invariant violation');
+        }
+
+        const line = this.currentToken.value;
+        const maybeDivertToken = this.peek();
+        const divert = maybeDivertToken?.type === 'divert' ? maybeDivertToken.target : null;
+        if (divert != null) {
+            // Make sure to consume divert in this case.
+            this.nextToken();
+        }
+        return {
+            type: 'unconditional_choice',
+            line,
+            divert,
+        };
+    }
+
+    private throwParseError(message: string, position: {line: number; column: number}): never {
+        throw new Error(`parse error on line ${position.line + 1}: ${message}`);
     }
 }
 
@@ -188,7 +253,22 @@ type SimpleChoice = {
     divert: string | null;
 }
 
-type AstNode = KnotNode | DivertNode | ActorLineNode | SimpleChoice;
+type UnconditionalChoice = {
+    type: 'unconditional_choice',
+    line: string;
+    divert: string | null;
+}
+
+type ControlFlowChoice = {
+    type: 'control_flow_choice',
+    condition: string;
+    consequent: UnconditionalChoice | ControlFlowChoice | null;
+    alternate: UnconditionalChoice | ControlFlowChoice | null;
+}
+
+type ComplexChoice = ControlFlowChoice | UnconditionalChoice;
+
+type AstNode = KnotNode | DivertNode | ActorLineNode | SimpleChoice | ComplexChoice;
 
 function oldParseYackFile(src: string, filename: string): string {
     const knots: Knot[] = [];
