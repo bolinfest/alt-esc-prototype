595a6f210853463fe554786438c7ec83a3b4e89c
Michael Bolin <bolinfest@gmail.com>
2022-06-11 11:27:50 -0700

    remove more unused code now that we use a proper tokenizer

--- src/yack/divert.ts
+++
@@ -1,4 +0,0 @@
-export function tryParseDivert(line: string): {knot: string} | null {
-  const match = line.match(/^\s*-> ([a-zA-Z][a-zA-Z0-9_]+)\b/);
-  return match != null ? {knot: match[1]} : null;
-}
